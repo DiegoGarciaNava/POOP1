1552ca329a5bbd473126b62c61b127bbc9360eb6
DiegoGarciaNava <90359547+DiegoGarciaNava@users.noreply.github.com>
2024-03-20 16:19:43 -0600

    Add files via upload
    Archivo java con el codigo de la primera practia

---
+++ POOP1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1;
+
+/**
+ *
+ * @author Diego
+ */
+public class POOP1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
